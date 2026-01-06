08ca823331db09d0dc5c86c2f49cecf318e4b461
Olin W. Calvin <olin.calvin@inl.gov>
2026-01-06 15:54:52 -0700

    Demonstrate mooseException hanging behavior

--- test/src/userobjects/MTUserObject.C
+++ test/src/userobjects/MTUserObject.C
@@ -37,7 +37,32 @@ MTUserObject::~MTUserObject() { delete[] _dyn_memory; }
 Real
 MTUserObject::doSomething() const
 {
+  std::vector<std::vector<int>> pids;
+  pids.resize(n_processors());
+  for (processor_id_type pid = 0; pid < n_processors(); ++pid)
+    pids[pid].resize(n_processors(), n_processors());
+  pids[processor_id()].assign(n_processors(), processor_id());
   // let's so something here, for example
+  printf("%s%d\n", "Test Print for PID: ", processor_id());
+  for (const auto & pid : pids)
+    for (const auto & proc_id : pid)
+      printf("%s%d%s%d\n", "Print 1 PID: ", proc_id, " on ", processor_id());
+  if (processor_id() == 1)
+  {
+    mooseException("Erroring on processor ", processor_id());
+  }
+  for (const auto & pid : pids)
+    for (const auto & proc_id : pid)
+      printf("%s%d%s%d\n", "Print 2 PID: ", proc_id, " on ", processor_id());
+  for (processor_id_type pid = 0; pid < n_processors(); ++pid)
+  {
+    comm().broadcast(pids[pid], pid);
+    if (pid == processor_id())
+      continue;
+  }
+  for (const auto & pid : pids)
+    for (const auto & proc_id : pid)
+      printf("%s%d%s%d\n", "Print 3 PID: ", proc_id, " on ", processor_id());
   return -2.;
 }
 
